65fef46cea5b56fdb702de7c12616bc3c18e94c8
OhsawaToumi <toumi.0038.1019@gmail.com>
2024-06-06 14:27:45 +0900

    second commit

--- app/src/main/java/jp/ac/gifu_u/info/toumi/myapplication/MainActivity.java
+++ app/src/main/java/jp/ac/gifu_u/info/toumi/myapplication/MainActivity.java
@@ -1,6 +1,10 @@
 package jp.ac.gifu_u.info.toumi.myapplication;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import android.view.Menu;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +12,28 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        setContentView(R.layout.activity_main);ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        Button b = (Button) findViewById(R.id.button2);
+        b.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        showToast("アプリを終了しました");
+        finish();
+
+    }
+
+    private void showToast(String アプリを終了しました) {
     }
 }
